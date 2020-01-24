f2507e03d801114cad86d715f1510b7bafe22548
Pirmayr <pirmayr@gmail.com>
2020-01-24 18:58:10 +0100

    Cleanup

--- Heaps/HeapMedian.cs
+++ Heaps/HeapMedian.cs
@@ -1,11 +1,6 @@
 // ReSharper disable TooWideLocalVariableScope
 // ReSharper disable JoinDeclarationAndInitializer
 
-// hugo 1
-
-using System;
-using System.Diagnostics;
-
 namespace Heaps
 {
     public static class HeapMedian
@@ -41,21 +36,9 @@ namespace Heaps
             return array[0];
         }
 
-        private static void Dump(int[] array)
-        {
-            if (array.Length <= 100)
-            {
-                foreach (int currentElement in array)
-                {
-                    Write(currentElement + " ");
-                }
-                WriteLine("");
-            }
-        }
-
         private static void Maxify1(int[] array, int length, int root)
         {
-            const int BlockSize = 10000;
+            const int BlockSize = 2;
             int currentRoot;
             int nextRoot;
             long child;
@@ -170,88 +153,12 @@ namespace Heaps
             }
         }
 
-        /*
-        public static int GetMedian(int[] array)
-        {
-            int length = array.Length;
-            int length2 = length / 2;
-            int length1 = length - length2;
-            int offset = 1 - 2 * length1;
-            int limit2 = (length - offset + 2) / 3;
-            int limit1 = (length1 + 1) / 3;
-            int root = length1 / 2 - 1;
-            int temporaryValue;
-            while (root >= 0)
-            {
-                while (true)
-                {
-                    Maxify1(array, length, root, length1, limit1);
-                    Minify1(array, root, length, limit2, length1, offset);
-                    if (array[root] <= array[root + length1])
-                    {
-                        break;
-                    }
-                    temporaryValue = array[root];
-                    array[root] = array[root + length1];
-                    array[root + length1] = temporaryValue;
-                }
-                --root;
-            }
-            return array[length1];
-        }
-        */
-
-        private static void Minify1(int[] array, int root, int high, int limit, int baseOffset, int offset)
-        {
-            int currentRoot;
-            int nextRoot;
-            int child;
-            nextRoot = root + baseOffset;
-            while (nextRoot < limit)
-            {
-                currentRoot = nextRoot;
-                child = nextRoot * 3 + offset;
-                switch (high - child)
-                {
-                case 2:
-                    nextRoot = array[child] < array[nextRoot] ? child : nextRoot;
-                    break;
-                case 3:
-                    nextRoot = array[child] < array[nextRoot] ? child : nextRoot;
-                    nextRoot = array[++child] < array[nextRoot] ? child : nextRoot;
-                    break;
-                default:
-                    nextRoot = array[child] < array[nextRoot] ? child : nextRoot;
-                    nextRoot = array[++child] < array[nextRoot] ? child : nextRoot;
-                    nextRoot = array[++child] < array[nextRoot] ? child : nextRoot;
-                    break;
-                }
-                if (nextRoot == currentRoot)
-                {
-                    break;
-                }
-                Swap(ref array[currentRoot], ref array[nextRoot]);
-            }
-        }
-
         private static void Swap(ref int a, ref int b)
         {
             ++swapCount;
             int temporaryValue = a;
             a = b;
             b = temporaryValue;
-        }
-
-        private static void Write(string formatString, params object[] values)
-        {
-            Console.Write(formatString, values);
-            Debug.Write(string.Format(formatString, values));
-        }
-
-        private static void WriteLine(string formatString, params object[] values)
-        {
-            Console.WriteLine(formatString, values);
-            Debug.WriteLine(formatString, values);
         }
     }
 }
--- Heaps/Program.cs
+++ Heaps/Program.cs
@@ -12,8 +12,6 @@ namespace Heaps
 
         public static void Main()
         {
-            Stopwatch heapMedianStopWatch = new Stopwatch();
-            Stopwatch quickSelectStopWatch = new Stopwatch();
             int[] array = new int[Length];
             for (int currentRepetition = 0; currentRepetition < Repetitions; ++currentRepetition)
             {
@@ -29,29 +27,7 @@ namespace Heaps
                 Dump(array);
                 WriteLine($"Swaps: {HeapMedian.swapCount}");
                 WriteLine($"Comparisons: {HeapMedian.comparisonCount}");
-                /*
-                arrayClone = (int[]) array.Clone();
-                heapMedianStopWatch.Start();
-                int heapMedianMedian = Heaps.GetMedian(arrayClone);
-                heapMedianStopWatch.Stop();
-                DumpResult(arrayClone, Repetitions, heapMedianMedian, Heaps.SwapCount);
-                arrayClone = (int[]) array.Clone();
-                quickSelectStopWatch.Start();
-                int quickSelectMedian = arrayClone[QuickSelect.GetMedianIndex(arrayClone, 0, arrayClone.Length - 1, arrayClone.Length / 2)];
-                quickSelectStopWatch.Stop();
-                DumpResult(array, Repetitions, quickSelectMedian, QuickSelect.SwapCount);
-                */
             }
-            /*
-            WriteLine($"Average time (Heaps): {heapMedianStopWatch.ElapsedMilliseconds / 1000.0 / Repetitions}");
-            WriteLine($"Comparison-factor (Heaps): {(double) Heaps.ComparisonCount / Length / Repetitions}");
-            // WriteLine($"No swap count (Heaps): {Heaps.noSwapCount}");
-            WriteLine($"Swap count (Heaps): {Heaps.swapCount}");
-            WriteLine($"Swap-factor (Heaps): {(double) Heaps.SwapCount / Length / Repetitions}");
-            WriteLine($"Average time (QuickSelect): {quickSelectStopWatch.ElapsedMilliseconds / 1000.0 / Repetitions}");
-            WriteLine($"Swap-factor (QuickSelect): {(double) QuickSelect.SwapCount / Length / Repetitions}");
-            WriteLine($"Algorithm-factor (HeadMedian/Quickselect): {(double) heapMedianStopWatch.ElapsedMilliseconds / quickSelectStopWatch.ElapsedMilliseconds}");
-            */
         }
 
         private static void Dump(int[] array)
@@ -63,36 +39,6 @@ namespace Heaps
                     Write(currentElement + " ");
                 }
                 WriteLine("");
-            }
-        }
-
-        private static void DumpResult(int[] array, int repetitions, int median, long swapCount)
-        {
-            if (repetitions < 10)
-            {
-                WriteLine(median.ToString());
-                GetCounts(array, median, out int smaller, out int bigger);
-                WriteLine(smaller.ToString());
-                WriteLine(bigger.ToString());
-                WriteLine(swapCount.ToString());
-                Dump(array);
-            }
-        }
-
-        private static void GetCounts(int[] array, int value, out int smaller, out int bigger)
-        {
-            smaller = 0;
-            bigger = 0;
-            foreach (int currentElement in array)
-            {
-                if (currentElement < value)
-                {
-                    ++smaller;
-                }
-                else
-                {
-                    ++bigger;
-                }
             }
         }
 
