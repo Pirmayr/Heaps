169901639c4c9cecc5a42c41c6ba6fee01f08e1c
Pirmayr <pirmayr@gmail.com>
2020-02-03 07:39:48 +0100

    Div. Änderungen.

--- Heaps/Program.cs
+++ Heaps/Program.cs
@@ -1,141 +1,149 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Threading;
 
-// ReSharper disable TooWideLocalVariableScope
 namespace Heaps
 {
-  public static class Example
-  {
-    public static void Main()
+    public static class Example
     {
-      // DumpBinaryTree(0, 0, 3);
-      // return;
-      const int Length = 100000000;
-      const int Repetitions = 1;
-      long comparisonsSum1 = 0;
-      long swapsSum1 = 0;
-      long comparisonsSum2 = 0;
-      long swapsSum2 = 0;
-      long comparisonsSum3 = 0;
-      long swapsSum3 = 0;
-      int[] array = new int[Length];
-      for (int currentRepetition = 0; currentRepetition < Repetitions; ++currentRepetition)
-      {
-        int seed = new Random().Next(array.Length * 4);
-        /*
-        PerformTest(array, seed, 1, Repetitions);
-        int maximum1 = Heaps.maximum;
-        comparisonsSum1 += Heaps.comparisons;
-        swapsSum1 += Heaps.swaps;
-        PerformTest(array, seed, 2, Repetitions);
-        int maximum2 = Heaps.maximum;
-        comparisonsSum2 += Heaps.comparisons;
-        swapsSum2 += Heaps.swaps;
-        */
-        PerformTest(array, seed, 3, Repetitions);
-        int maximum3 = Heaps.maximum;
-        comparisonsSum3 += Heaps.comparisons;
-        swapsSum3 += Heaps.swaps;
-        /*
-        if (maximum3 != maximum1)
+        public static void Main()
         {
-          WriteLine($"Error; Seed: {seed}");
-          break;
+            const int Repetitions = 1;
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+            List<AlgorithmInformation> algorithmInformations = new List<AlgorithmInformation> {new AlgorithmInformation(Heaps.SinkStandard), new AlgorithmInformation(Heaps.SinkWeakStandard), new AlgorithmInformation(Heaps.SinkWeak)};
+            for (int length = 10000000; length <= 10000000; length *= 2)
+            {
+                foreach (AlgorithmInformation currentAlgorithmInformation in algorithmInformations)
+                {
+                    currentAlgorithmInformation.Reset();
+                }
+                int[] array = new int[length];
+                for (int currentRepetition = 0; currentRepetition < Repetitions; ++currentRepetition)
+                {
+                    int seed = new Random().Next();
+                    foreach (AlgorithmInformation currentAlgorithmInformation in algorithmInformations)
+                    {
+                        TestMaxify(array, seed, currentAlgorithmInformation.SinkMethod, currentRepetition, ref currentAlgorithmInformation.maxifyComparisons, ref currentAlgorithmInformation.maxifySwaps);
+                        TestSink(array, seed, currentAlgorithmInformation.SinkMethod, currentRepetition, ref currentAlgorithmInformation.sinkComparisons, ref currentAlgorithmInformation.sinkSwaps);
+                    }
+                }
+                foreach (AlgorithmInformation currentAlgorithmInformation in algorithmInformations)
+                {
+                    currentAlgorithmInformation.maxifyComparisonValues.Add(length, (double) currentAlgorithmInformation.maxifyComparisons / Repetitions);
+                    currentAlgorithmInformation.maxifySwapValues.Add(length, (double) currentAlgorithmInformation.maxifySwaps / Repetitions);
+                    currentAlgorithmInformation.sinkComparisonValues.Add(length, (double) currentAlgorithmInformation.sinkComparisons / Repetitions);
+                    currentAlgorithmInformation.sinkSwapValues.Add(length, (double) currentAlgorithmInformation.sinkSwaps / Repetitions);
+                    Console.WriteLine($"Maxify; Average Swaps: {currentAlgorithmInformation.maxifySwaps / Repetitions} Average Comparisons: {currentAlgorithmInformation.maxifyComparisons / Repetitions}");
+                    Console.WriteLine($"Sink; Average Swaps: {currentAlgorithmInformation.sinkSwaps / Repetitions} Average Comparisons: {currentAlgorithmInformation.sinkComparisons / Repetitions}");
+                }
+            }
+            foreach (AlgorithmInformation currentAlgorithmInformation in algorithmInformations)
+            {
+                Console.WriteLine($"Algorithm {currentAlgorithmInformation.SinkMethod.Method.Name}:");
+                Console.WriteLine("Sink-Comparisons:");
+                foreach (KeyValuePair<int, double> currentValue in currentAlgorithmInformation.sinkComparisonValues)
+                {
+                    Console.WriteLine($"{currentValue.Key} {currentValue.Value}");
+                }
+                Console.WriteLine("Maxify-Comparisons:");
+                foreach (KeyValuePair<int, double> currentValue in currentAlgorithmInformation.maxifyComparisonValues)
+                {
+                    Console.WriteLine($"{currentValue.Key} {currentValue.Value}");
+                }
+            }
         }
-        */
-      }
-      WriteLine($"Average Swaps: {swapsSum1 / Repetitions} Average Comparisons: {comparisonsSum1 / Repetitions}");
-      WriteLine($"Average Swaps: {swapsSum2 / Repetitions} Average Comparisons: {comparisonsSum2 / Repetitions}");
-      WriteLine($"Average Swaps: {swapsSum3 / Repetitions} Average Comparisons: {comparisonsSum3 / Repetitions}");
-    }
 
-    private static void Dump(int[] array)
-    {
-      if (array.Length <= 100)
-      {
-        foreach (int currentElement in array)
+        private static void Dump(int[] array)
         {
-          Write(currentElement + " ");
+            if (array.Length <= 60)
+            {
+                for (int i = 0; i < array.Length; ++i)
+                {
+                    Console.Write("{0,3}", i);
+                }
+                Console.WriteLine("");
+                foreach (int currentElement in array)
+                {
+                    Console.Write("{0,3}", currentElement);
+                }
+                Console.WriteLine("");
+            }
         }
-        WriteLine("");
-      }
-    }
 
-    private static void DumpBinaryTree(int startNumber, int currentLevel, int maximalLevel)
-    {
-      if (currentLevel <= maximalLevel)
-      {
-        if (startNumber == 0)
+        private static void InitializeArray(int[] array, int seed)
         {
-          WriteLine(0.ToString());
-          Write(new string('-', currentLevel + 1));
-          WriteLine(1.ToString());
-          DumpBinaryTree(1, currentLevel + 1, maximalLevel);
+            Random random = new Random(seed);
+            int length = array.Length;
+            for (int i = 0; i < length; ++i)
+            {
+                array[i] = random.Next();
+            }
         }
-        else
+
+        private static int TestMaxify(int[] array, int seed, SinkMethod sinkMethod, int repetitionIndex, ref long comparisonsSum, ref long swapsSum)
         {
-          Write(new string('-', currentLevel + 1));
-          WriteLine((startNumber + 1).ToString());
-          DumpBinaryTree(4 * startNumber + 1, currentLevel + 1, maximalLevel);
-          ++startNumber;
-          Write(new string('-', currentLevel + 1));
-          WriteLine((startNumber + 1).ToString());
-          DumpBinaryTree(4 * startNumber + 1, currentLevel + 1, maximalLevel);
-          ++startNumber;
-          Write(new string('-', currentLevel + 1));
-          WriteLine((startNumber + 1).ToString());
-          DumpBinaryTree(4 * startNumber + 1, currentLevel + 1, maximalLevel);
-          ++startNumber;
-          Write(new string('-', currentLevel + 1));
-          WriteLine((startNumber + 1).ToString());
-          DumpBinaryTree(4 * startNumber + 1, currentLevel + 1, maximalLevel);
+            InitializeArray(array, seed);
+            if (repetitionIndex == 0)
+            {
+                Dump(array);
+            }
+            Heaps.Maxify(array, sinkMethod);
+            if (repetitionIndex == 0)
+            {
+                Dump(array);
+                Console.WriteLine($"Maximum: {Heaps.maximum} Seed: {seed} Swaps: {Heaps.swaps} Comparisons: {Heaps.comparisons}", new object[0]);
+            }
+            comparisonsSum += Heaps.comparisons;
+            swapsSum += Heaps.swaps;
+            return Heaps.maximum;
         }
-      }
-    }
 
-    private static void InitializeArray(int[] array, int seed)
-    {
-      Random random = new Random(seed);
-      int length = array.Length;
-      for (int i = 0; i < length; ++i)
-      {
-        array[i] = random.Next(length * 16);
-      }
-    }
+        private static int TestSink(int[] array, int seed, SinkMethod sinkMethod, int repetitionIndex, ref long comparisonsSum, ref long swapsSum)
+        {
+            int value = new Random(seed * 2).Next(array.Length);
+            array[0] = value;
+            if (repetitionIndex == 0)
+            {
+                Dump(array);
+            }
+            Heaps.Sink(array, sinkMethod);
+            if (repetitionIndex == 0)
+            {
+                Dump(array);
+                Console.WriteLine($"Maximum: {Heaps.maximum} Seed: {seed} Swaps: {Heaps.swaps} Comparisons: {Heaps.comparisons}", new object[0]);
+            }
+            comparisonsSum += Heaps.comparisons;
+            swapsSum += Heaps.swaps;
+            return Heaps.maximum;
+        }
 
-    private static void PerformTest(int[] array, int seed, int testIndex, int repetitions)
-    {
-      InitializeArray(array, seed);
-      if (repetitions == 1)
-      {
-        Dump(array);
-      }
-      switch (testIndex)
-      {
-        case 1:
-          Heaps.MaxifyStandard(array);
-          break;
-        case 2:
-          Heaps.MaxifyWeakStandard(array);
-          break;
-        case 3:
-          Heaps.MaxifyWeak(array);
-          break;
-      }
-      if (repetitions == 1)
-      {
-        Dump(array);
-        WriteLine($"Maximum: {Heaps.maximum} Seed: {seed} Swaps: {Heaps.swaps} Comparisons: {Heaps.comparisons}");
-      }
-    }
+        private class AlgorithmInformation
+        {
+            public long maxifyComparisons;
+            public long maxifySwaps;
+            public long sinkComparisons;
+            public long sinkSwaps;
 
-    private static void Write(string formatString, params object[] values)
-    {
-      Console.Write(formatString, values);
-    }
+            public readonly Dictionary<int, double> maxifyComparisonValues = new Dictionary<int, double>();
+            public readonly Dictionary<int, double> maxifySwapValues = new Dictionary<int, double>();
+            public readonly Dictionary<int, double> sinkComparisonValues = new Dictionary<int, double>();
+            public readonly Dictionary<int, double> sinkSwapValues = new Dictionary<int, double>();
 
-    private static void WriteLine(string formatString, params object[] values)
-    {
-      Console.WriteLine(formatString, values);
+            public SinkMethod SinkMethod { get; }
+
+            public AlgorithmInformation(SinkMethod sinkMethod)
+            {
+                SinkMethod = sinkMethod;
+            }
+
+            public void Reset()
+            {
+                maxifyComparisons = 0;
+                maxifySwaps = 0;
+                sinkComparisons = 0;
+                sinkSwaps = 0;
+            }
+        }
     }
-  }
 }
